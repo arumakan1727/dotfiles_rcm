f9bc7d04a64a2d4af95de5f3dc2499a0b7df8fec
arumakan1727 <aluminumCan7271@gmail.com>
2021-06-14 19:18:31 +0900

    fix

--- config/zsh/external_tool_init.zsh
+++ config/zsh/external_tool_init.zsh
@@ -3,6 +3,6 @@
 # External tool initialization
 #======================================================================================
 
-(command -v pyenv > /dev/null 2>&1) && eval "$(pyenv init -)"
+(command -v pyenv > /dev/null 2>&1) && eval "$(pyenv init --path)"
 (command -v direnv > /dev/null 2>&1) && eval "$(direnv hook zsh)"
 (command -v starship > /dev/null 2>&1) && eval "$(starship init zsh)"
