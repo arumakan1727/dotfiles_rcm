b76ae25b6c4d0c8eda1fb7f6ddad13c9362c4f9c
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2021-08-12 09:15:19 +0900

    add: `truecolor_test` command

--- config/zsh/function.zsh
+++ config/zsh/function.zsh
@@ -17,6 +17,22 @@ function ascii_color_code() {
   seq 30 47 | xargs -n 1 -i{} printf "\x1b[%dm#\x1b[0m %d\n" {} {}
 }
 
+function truecolor_test() {
+  awk 'BEGIN{
+    s="/\\/\\/\\/\\/\\"; s=s s s s s s s s;
+    for (colnum = 0; colnum<77; colnum++) {
+        r = 255-(colnum*255/76);
+        g = (colnum*510/76);
+        b = (colnum*255/76);
+        if (g>255) g = 510-g;
+        printf "\033[48;2;%d;%d;%dm", r,g,b;
+        printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+        printf "%s\033[0m", substr(s,colnum+1,1);
+    }
+    printf "\n";
+  }'
+}
+
 function find_no_new_line_at_end_of_file() {
   find * -type f -print0 | xargs -0 -L1 bash -c 'test "$(tail -c 1 "$0")" && echo "No new line at end of $0"'
 }
